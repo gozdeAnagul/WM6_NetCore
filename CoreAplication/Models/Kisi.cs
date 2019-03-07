f862989dd0fca854595e4ac2336cd4b5c0613f9c
Gözde Anagül <gozde.anagul@outlook.com>
2019-03-07 11:58:21 +0300

    categoru controller and add

---
+++ CoreAplication/Models/Kisi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAplication.Models
+{
+    public class Kisi
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string  Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ CoreAplication/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAplication.Models
+{
+    [Table("Urunler")]
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, StringLength(50), Column("UrunAdi")]
+        public string ProductName { get; set; }
+        [Column("Fİyat")]
+        public decimal UnitPrice { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
